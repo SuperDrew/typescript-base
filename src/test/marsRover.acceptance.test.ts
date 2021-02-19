b079a9b1afcac0163153175a1a074949bdc42601
Geir Gulbrandsen <geir.gulbrandsen@cazoo.co.uk>
2021-02-19 15:02:10 +0000

    mob next [ci-skip]

--- src/test/marsRover.acceptance.test.ts
+++ src/test/marsRover.acceptance.test.ts
@@ -1,7 +1,7 @@
-function sendCommandsToRover(commands: string) {
-    return undefined;
-}
-describe('Mars rover', () => {
+// function sendCommandsToRover(commands: string) {
+//     return undefined;
+// }
+describe.skip('Mars rover', () => {
     describe('given the rover is at 0:0:N in a grid 10x10 with no obstacle', () => {
         describe('with input MMRMMLM', () => {
             it('should gives output 2:3:N', () => {
--- src/test/marsRover.test.ts
+++ src/test/marsRover.test.ts
@@ -1,12 +1,21 @@
 function sendCommandsToRover(commands: string) {
-    return undefined;
+    return '0:0:N';
 }
+
 describe('Mars rover', () => {
     describe('given the rover is at 0:0:N in a grid 10x10 with no obstacle', () => {
         describe('with input empty', () => {
             it('should gives output 0:0:N', () => {
                 const commands = '';
                 const expected = '0:0:N';
+
+                expect(sendCommandsToRover(commands)).toBe(expected);
+            });
+        });
+        describe('with input "M"', () => {
+            it('should gives output 0:1:N', () => {
+                const commands = 'M';
+                const expected = '0:1:N';
 
                 expect(sendCommandsToRover(commands)).toBe(expected);
             });
