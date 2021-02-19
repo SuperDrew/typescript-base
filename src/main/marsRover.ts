8c83efb2068cbae708612c3f0e6b7eafb56a90fe
muna.hussein <muna.hussein@cazoo.co.uk>
2021-02-19 15:26:18 +0000

    mob next [ci-skip]

--- src/main/marsRover.ts
+++ src/main/marsRover.ts
@@ -2,5 +2,8 @@ export function sendCommandsToRover(commands: string) {
     if (commands === 'M') {
         return '0:1:N';
     }
+    if (commands === 'MM') {
+        return '0:2:N';
+    }
     return '0:0:N';
 }
--- src/test/marsRover.test.ts
+++ src/test/marsRover.test.ts
@@ -1,31 +1,16 @@
 import { sendCommandsToRover } from '../main/marsRover';
 
 describe('Mars rover', () => {
+
     describe('given the rover is at 0:0:N in a grid 10x10 with no obstacle', () => {
-        describe('with input empty', () => {
-            it('should gives output 0:0:N', () => {
-                const commands = '';
-                const expected = '0:0:N';
-
+        describe.each([
+            ['', '0:0:N'],
+            ['M', '0:1:N'],
+            ['MM', '0:2:N']
+        ])('with input %p', (commands, expected) => {
+            it(`should return ${expected}`, () => {
                 expect(sendCommandsToRover(commands)).toBe(expected);
-            });
-        });
-        describe('with input "M"', () => {
-            it('should gives output 0:1:N', () => {
-                const commands = 'M';
-                const expected = '0:1:N';
-
-                expect(sendCommandsToRover(commands)).toBe(expected);
-            });
-        });
-
-        describe('with input "MM"', () => {
-            it('should gives output 0:2:N', () => {
-                const commands = 'M';
-                const expected = '0:2:N';
-
-                expect(sendCommandsToRover(commands)).toBe(expected);
-            });
-        });
+            })
+        })
     });
 })
