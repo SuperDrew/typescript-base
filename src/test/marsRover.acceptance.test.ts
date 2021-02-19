287cce411284487f401787494a8d1c41a0fe8b39
Vittorio Guerriero <vittorio.gue@gmail.com>
2021-02-19 14:44:31 +0000

    mob next [ci-skip]

---
+++ src/test/marsRover.acceptance.test.ts
@@ -0,0 +1,16 @@
+function sendCommandsToRover(commands: string) {
+    return undefined;
+}
+
+describe('Mars rover', () => {
+    describe('given the rover is at 0:0:N in a grid 10x10 with no obstacle', () => {
+        describe('with input MMRMMLM', () => {
+            it('should gives output 2:3:N', () => {
+                const commands = 'MMRMMLM';
+                const expected = '2:3:N';
+
+                expect(sendCommandsToRover(commands)).toBe(expected);
+            });
+        });
+    });
+})
