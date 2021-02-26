375c114dbdbc51a2d6daf9a47c844b3d352f116d
Vittorio Guerriero <vittorio.gue@gmail.com>
2021-02-26 15:33:33 +0000

    mob next [ci-skip]

--- src/main/marsRover.ts
+++ src/main/marsRover.ts
@@ -10,13 +10,23 @@ function move(direction: "N" | "E" | "S" | "W", xPos: number, yPos: number) {
     } else {
         yPos += 1;
     }
-    return {xPos, yPos};
+    return {x: xPos, y: yPos};
+}
+
+export class RoverPosition {
+    public x: number;
+    public y: number;
+    public direction: Direction;
+
+    constructor(x = 0, y = 0, d: Direction = 'N') {
+        this.x = x;
+        this.y = y;
+        this.direction = d;
+    }
 }
 
 export function sendCommandsToRover(commands: string) {
-    let xPos = 0;
-    let yPos = 0;
-    let direction : Direction = 'N';
+    const roverPosition = new RoverPosition();
 
     const clockwiseRotation : Record <Direction, Direction> = {
         'N':'E',
@@ -32,17 +42,18 @@ export function sendCommandsToRover(commands: string) {
         'E':'N'
     }
 
+
     commands.split('').forEach((command: string) => {
         if (command === 'R') {
-            direction = clockwiseRotation[direction]
+            roverPosition.direction = clockwiseRotation[roverPosition.direction]
         } else if (command === 'L') {
-            direction = antiClockwiseRotation[direction]
+            roverPosition.direction = antiClockwiseRotation[roverPosition.direction]
         } else if (command === 'M') {
-            const __ret = move(direction, xPos, yPos);
-            xPos = __ret.xPos;
-            yPos = __ret.yPos;
+            const {x, y} = move(roverPosition.direction, roverPosition.x, roverPosition.y);
+            roverPosition.x = x;
+            roverPosition.y = y;
         }
     })
 
-    return `${xPos}:${yPos}:${direction}`;
+    return `${roverPosition.x}:${roverPosition.y}:${roverPosition.direction}`;
 }
