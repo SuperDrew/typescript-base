4513f0c39a660df87ad4e4134cd0384b9bf80a00
Andrew Fraser <a.vector@gmail.com>
2021-06-03 11:56:08 +0100

    chore - cleanup tests

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -2,14 +2,14 @@ import {convertToBooleanWithDefaultTrue} from "../main/example";
 
 describe('example test', () => {
     it('should transform a boolean true string to an actual boolean', () => {
-        expect(convertToBooleanWithDefaultTrue('true')).toStrictEqual(true);
+        expect(convertToBooleanWithDefaultTrue('true')).toBe(true);
     });
 
     it('should transform a boolean false string to an actual boolean', () => {
-        expect(convertToBooleanWithDefaultTrue('false')).toStrictEqual(false);
+        expect(convertToBooleanWithDefaultTrue('false')).toBe(false);
     });
 
-    it('should transform sslEnabled to true if the input string isnt true or false', () => {
-        expect(convertToBooleanWithDefaultTrue('sdfgkhusdf')).toStrictEqual(true);
+    it('should transform boolean to true if the input string isnt true or false', () => {
+        expect(convertToBooleanWithDefaultTrue('sdfgkhusdf')).toBe(true);
     });
 })
